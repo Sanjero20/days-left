378e620f00373322b21043370d2c48013ded3bc3
Sanjero20 <gelosantos72@gmail.com>
2024-08-01 19:41:06 +0800

    feat: display current date

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
-console.log('Hello world');
+import './style.css';
+
+enum Months {
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+}
+
+// Get current date
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+
+// Calculate difference between today and before the year ends
+
+// Display to screen
+const h1 = document.getElementById('days') as HTMLElement;
+h1.textContent = `${Months[month]} ${day}, ${year}`;
