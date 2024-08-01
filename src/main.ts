5ea669ae231520b9867599e0cdfd0d53aea423ea
Sanjero20 <gelosantos72@gmail.com>
2024-08-01 22:09:34 +0800

    feat: display remaining days left

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,18 @@ const year = date.getFullYear();
 const month = date.getMonth();
 const day = date.getDate();
 
-// Calculate difference between today and before the year ends
+// Get the total days per each month of the remaining
+let remainingDays = 0;
+for (let i = month; i <= 11; i++) {
+  const daysPerMonth = new Date(year, i, 0).getDate();
+  remainingDays += daysPerMonth;
+}
+
+remainingDays = remainingDays - day;
 
 // Display to screen
-const h1 = document.getElementById('days') as HTMLElement;
+const h1 = document.getElementById('date') as HTMLElement;
+const h2 = document.getElementById('remaining') as HTMLElement;
+
+h2.textContent = `${remainingDays} days left in this year`;
 h1.textContent = `${Months[month]} ${day}, ${year}`;
